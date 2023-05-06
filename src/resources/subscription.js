ede3a161a609ed86b7d466d8755ff43a6e833ae1
santimenendez98 <santi.mm11@gmail.com>
2023-05-06 13:58:41 -0300

    MR-08: Finished create subscription request and fix some things of the delete request

--- src/resources/subscription.js
+++ src/resources/subscription.js
@@ -11,21 +11,38 @@ router.post('/create', (req, res) => {
   const newSubscription = req.body;
   const requireProps = ['id', 'class', 'date'];
   const isValid = Object.keys(newSubscription).every((prop) => requireProps.includes(prop));
-  const id = +newSubscription.id;
+  const idClass = newSubscription.id;
+  const nameClass = newSubscription.class;
+  const dateClass = newSubscription.date;
+  const onlyLetters = /^[a-zA-Z]+$/;
+  const formatDate = /^\d{2}\/\d{2}\/\d{4}$/;
 
   if (isValid) {
-    if (!Number.isNaN(id)) {
-      classes.push(newSubscription);
-      fs.writeFile('src/data/subscription.json', JSON.stringify(classes, null, 2), (error) => {
-        if (error) {
-          res.status(401).send('Subscription cannot be created');
-        } else {
-          res.status(200).send('Subscription created');
-        }
-      });
-    } else {
-      res.send('Invalid ID');
+    if (typeof idClass !== 'number') {
+      res.status(422).send('ID has to be a Number');
+      return;
     }
+    if (!onlyLetters.test(nameClass)) {
+      res.status(422).send('Class has to be letters');
+      return;
+    }
+    if (String(nameClass).length < 4) {
+      res.status(422).send('Minimum character for name is 4');
+      return;
+    }
+    if (!formatDate.test(dateClass)) {
+      res.status(422).send('Format date has to be a mm/dd/year');
+      return;
+    }
+
+    classes.push(newSubscription);
+    fs.writeFile('src/data/subscription.json', JSON.stringify(classes, null, 2), (error) => {
+      if (error) {
+        res.status(401).send('Subscription cannot be created');
+      } else {
+        res.status(200).send('Subscription created');
+      }
+    });
   } else {
     res.status(404).send('Invalid subscription data');
   }
@@ -39,12 +56,12 @@ router.delete('/delete/:id', (req, res) => {
   const exist = classes.find((sub) => sub.id.toString() === subscriptionDelete);
 
   if (Number.isNaN(subscriptionNumber)) {
-    res.send('ID has be a number');
+    res.status(422).send('ID has be a number');
     return;
   }
 
   if (!exist) {
-    res.send('ID are not found');
+    res.status(422).send('ID are not found');
     return;
   }
 
