a722f2d93eabd279843e5bb620736e3f0c16dc8e
EmaBaldor <emabaldor@gmail.com>
2023-05-06 21:08:19 -0300

    MR-08: Added put method

--- src/resources/trainer.js
+++ src/resources/trainer.js
@@ -3,10 +3,37 @@ import express from 'express';
 import trainers from '../data/trainer.json';
 
 const router = express.Router();
+const fs = require('fs');
 
-// get
+// GET
 router.get('/', (req, res) => {
   res.send(trainers);
+});
+
+// PUT
+router.put('/:id', (req, res) => {
+  const newData = req.body; // Obtén los nuevos datos del cuerpo de la solicitud
+  const trainerId = req.params.id;
+  const foundTrainer = trainers.findIndex((trainer) => trainer.id.toString() === trainerId);
+
+  // Validate empty fields
+  const emptyFields = Object.keys(newData).filter((key) => newData[key] === '');
+  if (emptyFields.length > 0) res.send(`Error: Fields '${emptyFields.join(', ')}' cannot be empty`);
+
+  if (!foundTrainer) {
+    res.send('Trainer not found!');
+  } else {
+    trainers[foundTrainer] = { ...trainers[foundTrainer], ...newData };
+
+    // write the changes in trainer.JSON
+    fs.writeFile('./src/data/trainer.json', JSON.stringify(trainers, null, 2), (err) => {
+      if (err) {
+        res.send('Error updating trainer data');
+      } else {
+        res.send(`Trainer id ${trainerId} data updated successfully `);
+      }
+    });
+  }
 });
 
 export default router;
