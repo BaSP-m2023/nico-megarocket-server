53080cbf41aad51db6bd2a539c37cd72de713085
santimenendez98 <santi.mm11@gmail.com>
2023-05-07 17:59:57 -0300

    MR-08: Fixed some things about status code and variable names

--- src/resources/subscription.js
+++ src/resources/subscription.js
@@ -14,37 +14,37 @@ router.post('/create', (req, res) => {
   const idClass = newSubscription.id;
   const nameClass = newSubscription.activity;
   const dateClass = newSubscription.date;
-  const onlyLetters = /^[a-zA-Z]+$/;
-  const formatDate = /^\d{2}\/\d{2}\/\d{4}$/;
+  const lettersRegex = /^[a-zA-Z]+$/;
+  const dateRegex = /^\d{2}\/\d{2}\/\d{4}$/;
 
   if (isValid) {
     if (typeof idClass !== 'number') {
-      res.status(422).send('ID has to be a Number');
+      res.status(400).send('ID has to be a Number');
       return;
     }
-    if (!onlyLetters.test(nameClass)) {
-      res.status(422).send('Activity has to be letters');
+    if (!lettersRegex.test(nameClass)) {
+      res.status(400).send('Activity has to be letters');
       return;
     }
     if (String(nameClass).length < 4) {
-      res.status(422).send('Minimum character for name is 4');
+      res.status(400).send('Minimum character for name is 4');
       return;
     }
-    if (!formatDate.test(dateClass)) {
-      res.status(422).send('Format date has to be a mm/dd/year');
+    if (!dateRegex.test(dateClass)) {
+      res.status(400).send('Format date has to be a mm/dd/year');
       return;
     }
 
     classes.push(newSubscription);
     fs.writeFile('src/data/subscription.json', JSON.stringify(classes, null, 2), (error) => {
       if (error) {
-        res.status(401).send('Subscription cannot be created');
+        res.status(500).send('Subscription cannot be created');
       } else {
         res.send('Subscription created');
       }
     });
   } else {
-    res.status(404).send('Invalid subscription data');
+    res.status(400).send('Invalid subscription data');
   }
 });
 
@@ -53,16 +53,16 @@ router.post('/create', (req, res) => {
 router.delete('/delete/:id', (req, res) => {
   const subscriptionDelete = req.params.id;
   const exist = classes.find((sub) => sub.id.toString() === subscriptionDelete);
-  const subscriptionFind = classes.filter((sub) => sub.id.toString() !== subscriptionDelete);
+  const newSubscriptionData = classes.filter((sub) => sub.id.toString() !== subscriptionDelete);
 
   if (!exist) {
-    res.status(422).send('ID are not found');
+    res.status(404).send('ID are not found');
     return;
   }
 
-  fs.writeFile('src/data/subscription.json', JSON.stringify(subscriptionFind, null, 2), (error) => {
+  fs.writeFile('src/data/subscription.json', JSON.stringify(newSubscriptionData, null, 2), (error) => {
     if (error) {
-      res.status(401).send('Subscription cannot be deleted');
+      res.status(500).send('Subscription cannot be deleted');
     } else {
       res.send('Subscription has been deleted');
     }
@@ -75,8 +75,8 @@ router.get('/:id', (req, res) => {
   const idSubscription = req.params.id;
   const exist = classes.filter((sub) => sub.id.toString() === idSubscription);
 
-  if (exist.length === 0) {
-    res.status(422).send('Subscritpion are not found');
+  if (!exist) {
+    res.status(404).send('Subscritpion are not found');
   } else {
     res.send(exist);
   }
@@ -91,7 +91,7 @@ router.get('/', (req, res) => {
   && sub.date.toString() === date);
 
   if (filterSubscription.length === 0) {
-    res.status(422).send('Subscription are not found');
+    res.status(404).send('Subscription are not found');
   } else {
     res.send(filterSubscription);
   }
@@ -102,14 +102,14 @@ router.get('/', (req, res) => {
 router.put('/edit/:id', (req, res) => {
   const getId = req.params.id;
   const requireProps = ['id', 'activity', 'date'];
-  const onlyLetters = /^[a-zA-Z]+$/;
-  const formatDate = /^\d{2}\/\d{2}\/\d{4}$/;
-  const findId = classes.find((sub) => sub.id.toString() === getId);
+  const lettersRegex = /^[a-zA-Z]+$/;
+  const dateRegex = /^\d{2}\/\d{2}\/\d{4}$/;
+  const foundId = classes.find((sub) => sub.id.toString() === getId);
   const newProps = Object.keys(req.body);
   const isDifferentProps = newProps.some((prop) => !requireProps.includes(prop));
 
-  if (!findId) {
-    res.status(422).send('Subscription are not found for edit');
+  if (!foundId) {
+    res.status(404).send('Subscription are not found for edit');
     return;
   }
 
@@ -118,21 +118,21 @@ router.put('/edit/:id', (req, res) => {
     return;
   }
 
-  findId.activity = req.body.activity;
-  findId.date = req.body.date;
+  foundId.activity = req.body.activity;
+  foundId.date = req.body.date;
 
-  if (!onlyLetters.test(findId.activity)) {
-    res.status(422).send('Activity has to be letters');
+  if (!lettersRegex.test(foundId.activity)) {
+    res.status(400).send('Activity has to be letters');
     return;
   }
 
-  if (!formatDate.test(findId.date)) {
-    res.status(422).send('Date format are invalid');
+  if (!dateRegex.test(foundId.date)) {
+    res.status(400).send('Date format are invalid');
     return;
   }
   fs.writeFile('src/data/subscription.json', JSON.stringify(classes, null, 2), (error) => {
     if (error) {
-      res.status(401).send('Subscription cannot be edited');
+      res.status(500).send('Subscription cannot be edited');
     } else {
       res.send('Subscription has been edited');
     }
