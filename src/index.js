62ccb761a9ae67a458942e1db122a48852d49294
Daniel Ninfi <dbninfi@gmail.com>
2023-05-08 19:12:08 -0300

    MR-08: Endpoints

--- src/index.js
+++ src/index.js
@@ -2,14 +2,15 @@
 import express from 'express';
 import cors from 'cors';
 
-// use "require" to import JSON files
 const admins = require('./data/admins.json');
+const classes = require('./resources/class');
 
 const app = express();
 const port = process.env.PORT || 4000;
 
 app.use(cors());
 app.use(express.json());
+app.use('/class', classes);
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
@@ -22,6 +23,6 @@ app.get('/admins', (req, res) => {
 });
 
 app.listen(port, () => {
-  // eslint-disable-next-line no-console
+// eslint-disable-next-line no-console
   console.log(`Example app listening on port ${port}`);
 });
--- src/resources/class.js
+++ src/resources/class.js
@@ -1,30 +1,57 @@
+const express = require('express');
+
 const fs = require('fs');
+const classes = require('../data/class.json');
 
-class Class {
-  constructor(filename) {
-    this.filename = filename;
+const router = express.Router();
+
+router.get('/:id', (req, res) => {
+  const { id } = req.params;
+  const valid = classes.filter((obj) => obj.id.toString() === id);
+  if (valid.length === 0) {
+    res.status(404).send('Class not found');
+  } else {
+    res.send(valid);
+  }
+});
+
+router.post('/:id', (req, res) => {
+  const newClass = req.body;
+  const { id } = newClass;
+  const existingClass = classes.find((c) => c.id === id);
+
+  if (existingClass) {
+    res.status(409).send('Class already exists');
+    return;
   }
 
-  async getAll() {
-    return JSON.parse(await fs.promises.readFile(this.filename, 'utf8'));
+  classes.push(newClass);
+  fs.writeFile('src/data/class.json', JSON.stringify(classes, null, 2), (error) => {
+    if (error) {
+      res.status(500).send('Class cannot be created');
+    } else {
+      res.send('Class has been created');
+    }
+  });
+});
+
+router.delete('/:id', (req, res) => {
+  const classDelete = req.params.id;
+  const existClass = classes.find((sub) => sub.id.toString() === classDelete);
+  const newClassData = classes.filter((sub) => sub.id.toString() !== classDelete);
+
+  if (!existClass) {
+    res.status(404).send('ID are not found');
+    return;
   }
 
-  async getById(id) {
-    const allItems = await this.getAll();
-    return allItems.find((item) => item.id === id);
-  }
+  fs.writeFile('src/data/class.json', JSON.stringify(newClassData, null, 2), (error) => {
+    if (error) {
+      res.status(500).send('Class cannot be deleted');
+    } else {
+      res.send('Class has been deleted');
+    }
+  });
+});
 
-  async create(newItem) {
-    const allItems = await this.getAll();
-    allItems.push(newItem);
-    await fs.promises.writeFile(this.filename, JSON.stringify(allItems));
-  }
-
-  async delete(id) {
-    const allItems = await this.getAll();
-    const updatedItems = allItems.filter((item) => item.id !== id);
-    await fs.promises.writeFile(this.filename, JSON.stringify(updatedItems));
-  }
-}
-
-module.exports = Class;
+module.exports = router;
