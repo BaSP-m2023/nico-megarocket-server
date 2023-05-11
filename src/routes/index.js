c403c8feded889fcb19f58069241009db0230322
santimenendez98 <santi.mm11@gmail.com>
2023-05-11 18:21:55 -0300

    MR-09-33: Added super-admins file on routes

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 
 const router = express.Router();
 
-const superAdmin = require('../controllers/super-admins');
+const superAdmin = require('./super-admins');
 
 router.use('/superadmin', superAdmin);
 
