ffe264ab6bb397c07b21505189e436a000883355
octavi01 <octaferreyraleon@gmail.com>
2023-05-15 00:09:48 -0300

    MR-09-21: added activity-delete code

--- src/controllers/activity.js
+++ src/controllers/activity.js
@@ -28,6 +28,26 @@ const updateActivity = (req, res) => {
     .catch((error) => res.status(400).json(error));
 };
 
+const deleteActivity = (req, res) => {
+  const { id } = req.params;
+
+  Activity.findByIdAndDelete(id)
+    .then((result) => {
+      if (!result) {
+        return res.status(404).json({
+          message: `Activity with the id: ${id} was not found, please try with another one`,
+          error: true,
+        });
+      }
+      return res.status(204).send();
+    })
+    .catch((error) => res.status(400).json({
+      message: 'Oops! There was an error!',
+      error,
+    }));
+};
+
 module.exports = {
   updateActivity,
+  deleteActivity,
 };
--- src/routes/activity.js
+++ src/routes/activity.js
@@ -2,7 +2,7 @@ const express = require('express');
 const activityController = require('../controllers/activity');
 const validations = require('../validations/activity');
 
-const router = express.router();
+const router = express.Router();
 
 router
   .delete('/:id', activityController.deleteActivity)
