a76c10c37289da2b1e9f83af7f279aff79b704d6
josedaniele <josedaniele493@gmail.com>
2023-05-15 12:54:05 -0300

    MR-09-10: error 404 getById added

--- src/controllers/admins.js
+++ src/controllers/admins.js
@@ -45,11 +45,19 @@ const getAdmins = (req, res) => {
 const getAdminsById = (req, res) => {
   const { id } = req.params;
   Admin.findById(id)
-    .then((admin) => res.status(200).json({
-      message: 'Admin found',
-      data: admin,
-      error: false,
-    }))
+    .then((admin) => {
+      if (admin) {
+        res.status(200).json({
+          message: 'Admin found',
+          data: admin,
+          error: false,
+        });
+      } else {
+        res.status(404).json({
+          message: 'Admin not found',
+        });
+      }
+    })
     .catch((error) => res.status(400).json({
       message: 'Error in the request',
       error,
