7f0e013df4ca31b6084292a7b8cfff2046b2f1ce
josedaniele <josedaniele493@gmail.com>
2023-05-18 15:08:31 -0300

    MR-35 : SetUp enviroment

---
+++ src/app.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const cors = require('cors');
+const router = require('./routes');
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use('/api', router);
+
+export default app;
---
+++ src/tests/gym.test.js
@@ -0,0 +1,4 @@
+// Test basico para evitar conflictos.Remplazarlos con sus test
+test('should pass a basic test', () => {
+  expect(1 + 1).toBe(2);
+});
