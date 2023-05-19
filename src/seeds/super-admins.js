49d5f7b77865a18cf82a1d9f82a6f0c661c7067b
octavi01 <octaferreyraleon@gmail.com>
2023-05-19 10:36:17 -0300

    MR-41: Create superAdmin seed

--- src/seeds/super-admins.js
+++ src/seeds/super-admins.js
@@ -1,1 +1,54 @@
-// seeds here
+import mongoose from 'mongoose';
+
+export default [
+  {
+    _id: new mongoose.Types.ObjectId('64667748fc13ae7f027631cc'),
+    email: 'probando1@gmail.com',
+    password: 'Password123',
+  },
+  {
+    _id: new mongoose.Types.ObjectId('64667748fc13ae7f058743dd'),
+    email: 'losperezgarcia@gmail.com',
+    password: 'Fuistelamadgalena123',
+  },
+  {
+    _id: new mongoose.Types.ObjectId('64667748fc13ae7f033343cb'),
+    email: 'notevagustar@gmail.com',
+    password: 'Vivauruguay123',
+  },
+  {
+    _id: new mongoose.Types.ObjectId('64667748fc13ae7f025473cb'),
+    email: 'eselnobayteresuena@gmail.com',
+    password: 'Sepusocachengoso123',
+  },
+  {
+    _id: new mongoose.Types.ObjectId('64667748fc13ae7f027454cb'),
+    email: 'theonewhoknocks@gmail.com',
+    password: 'Breakingbad123',
+  },
+  {
+    _id: new mongoose.Types.ObjectId('64667748fc13ae7f027666cb'),
+    email: 'estemaileselmio@gmail.com',
+    password: 'Yestaesmicontraseña123',
+  },
+  {
+    _id: new mongoose.Types.ObjectId('64667748fc13ae7f027777cb'),
+    email: 'muybasicotodo@gmail.com',
+    password: '123Cambieelformato',
+  },
+  {
+    _id: new mongoose.Types.ObjectId('64667748fc13ae7f027888cb'),
+    email: 'masfacilconelmock@gmail.com',
+    password: 'Peromenosdivertido5500',
+  },
+  {
+    _id: new mongoose.Types.ObjectId('64667748fc13ae7f027999cb'),
+    email: 'tutancamon@gmail.com',
+    password: 'laspiramides425',
+  },
+  {
+    _id: new mongoose.Types.ObjectId('64667748fc13ae7f027020cb'),
+    email: 'thatsmyemail@gmail.com',
+    password: 'mePusebilingue444',
+  },
+];
