839d6365a45f592d5fc3f928bbb058c178b2f9b9
nazarena pantanetti <nazapantanetti@gmail.com>
2023-05-19 12:39:49 -0300

    MR-45: Create activity seed

--- src/seeds/activity.js
+++ src/seeds/activity.js
@@ -1,1 +1,64 @@
-// seeds here
+import mongoose from 'mongoose';
+
+export default [
+  {
+    _id: new mongoose.Types.ObjectId('6466ggl7fc13ae78027631cc'),
+    name: 'Hayden',
+    description: 'volley',
+    isActive: true,
+  },
+  {
+    _id: new mongoose.Types.ObjectId('3k66ggl7fc13ae7bhe4631cc'),
+    name: 'Gabie',
+    description: 'futbbol',
+    isActive: false,
+  },
+  {
+    _id: new mongoose.Types.ObjectId('6409xsl7fc13ae78027631cc'),
+    name: 'Anissa',
+    description: 'spinning',
+    isActive: false,
+  },
+  {
+    _id: new mongoose.Types.ObjectId('0409ggl7fc2q3ae78027631cc'),
+    name: 'Conney',
+    description: 'swimming',
+    isActive: true,
+  },
+  {
+    _id: new mongoose.Types.ObjectId('6987kggl7fc13ae78027631cc'),
+    name: 'Tamma',
+    description: 'running',
+    isActive: true,
+  },
+  {
+    _id: new mongoose.Types.ObjectId('7466gpl7fc13ae780jkyu1cc'),
+    name: 'Mohandis',
+    description: 'handball',
+    isActive: false,
+  },
+  {
+    _id: new mongoose.Types.ObjectId('762195cdl7fc13ae78027631cc'),
+    name: 'Anetta',
+    description: 'muscle',
+    isActive: false,
+  },
+  {
+    _id: new mongoose.Types.ObjectId('09ft45gl7fc13ae7807631cc'),
+    name: 'Austin',
+    description: 'abs',
+    isActive: true,
+  },
+  {
+    _id: new mongoose.Types.ObjectId('56drsgl7fc13ae78027631cc'),
+    name: 'Essie',
+    description: 'boxing',
+    isActive: true,
+  },
+  {
+    _id: new mongoose.Types.ObjectId('6494ggl7fc13ae78plvx31cc'),
+    name: 'Sabina',
+    description: 'funtional',
+    isActive: true,
+  },
+];
