f110d0a14edcd0bddad105bcbeb598c572cc821f
enriqueanastasini <enrianastasini@gmail.com>
2023-05-21 20:25:23 -0300

    MR-10-57: creation os subscription seed

--- src/seeds/subscription.js
+++ src/seeds/subscription.js
@@ -1,1 +1,118 @@
-// seeds here
+import mongoose from 'mongoose';
+
+export default [
+  {
+    _id: new mongoose.Types.ObjectId('6467f535fc13ae553d753ae8'),
+    classId: '6463f08ddc8d9113c7655389',
+    members: '6465539d02739f6df0878cd3',
+    date: new Date('2023-04-13'),
+  },
+  {
+    _id: new mongoose.Types.ObjectId('6467f535fc13ae553d753ae9'),
+    classId: '6463a669815a24e23dde0106',
+    members: '64650c82fb9b0234915f4be7',
+    date: new Date('2022-10-29'),
+  },
+  {
+    _id: new mongoose.Types.ObjectId('6467f535fc13ae553d753aea'),
+    classId: '6463a669815a24e23dde0106',
+    members: '6465539d02739f6df0878cd3',
+    date: new Date('2022-08-03'),
+  },
+  {
+    _id: new mongoose.Types.ObjectId('6467f535fc13ae553d753aeb'),
+    classId: '6463a6301a8047b06aece8d1',
+    members: '6465931d4fcb63fdd73b2891',
+    date: new Date('2022-11-28'),
+  },
+  {
+    _id: new mongoose.Types.ObjectId('6467f535fc13ae553d753aec'),
+    classId: '6462d571d0664289fc7c291b',
+    members: '646577dd1d9fbe26edc4bb60',
+    date: new Date('2023-04-16'),
+  },
+  {
+    _id: new mongoose.Types.ObjectId('6467f535fc13ae553d753aed'),
+    classId: '6463f08ddc8d9113c7655389',
+    members: '64667a84a1350cc48de5d447',
+    date: new Date('2023-05-15'),
+  },
+  {
+    _id: new mongoose.Types.ObjectId('6467f535fc13ae553d753aee'),
+    classId: '6464002acb97f66a10ee6fc4',
+    members: '64650c82fb9b0234915f4be7',
+    date: new Date('2023-02-21'),
+  },
+  {
+    _id: new mongoose.Types.ObjectId('6467f535fc13ae553d753aef'),
+    classId: '6463a669815a24e23dde0106',
+    members: '646577dd1d9fbe26edc4bb60',
+    date: new Date('2022-12-03'),
+  },
+  {
+    _id: new mongoose.Types.ObjectId('6467f535fc13ae553d753af0'),
+    classId: '6465905f4fcb63fdd73b288d',
+    members: '6465931d4fcb63fdd73b2891',
+    date: new Date('2023-01-21'),
+  },
+  {
+    _id: new mongoose.Types.ObjectId('6467f535fc13ae553d753af1'),
+    classId: '64666b861fb62f8171bb6519',
+    members: '6465931d4fcb63fdd73b2891',
+    date: new Date('2022-10-12'),
+  },
+  {
+    _id: new mongoose.Types.ObjectId('6467f535fc13ae553d753af2'),
+    classId: '6466781ca1350cc48de5d436',
+    members: '646577dd1d9fbe26edc4bb60',
+    date: new Date('2022-08-02'),
+  },
+  {
+    _id: new mongoose.Types.ObjectId('6467f535fc13ae553d753af3'),
+    classId: '6463f08ddc8d9113c7655389',
+    members: '6465535102739f6df0878ccf',
+    date: new Date('2022-11-07'),
+  },
+  {
+    _id: new mongoose.Types.ObjectId('6467f535fc13ae553d753af4'),
+    classId: '6463f08ddc8d9113c7655389',
+    members: '646577dd1d9fbe26edc4bb60',
+    date: new Date('2023-03-13'),
+  },
+  {
+    _id: new mongoose.Types.ObjectId('6467f535fc13ae553d753af5'),
+    classId: '6466781ca1350cc48de5d436',
+    members: '64667a84a1350cc48de5d447',
+    date: new Date('2023-01-27'),
+  },
+  {
+    _id: new mongoose.Types.ObjectId('6467f535fc13ae553d753af6'),
+    classId: '6464002acb97f66a10ee6fc4',
+    members: '6465535102739f6df0878ccf',
+    date: new Date('2023-01-02'),
+  },
+  {
+    _id: new mongoose.Types.ObjectId('6467f535fc13ae553d753af7'),
+    classId: '6466782ea1350cc48de5d438',
+    members: '6465931d4fcb63fdd73b2891',
+    date: new Date('2023-04-28'),
+  },
+  {
+    _id: new mongoose.Types.ObjectId('6467f535fc13ae553d753af8'),
+    classId: '6463a669815a24e23dde0106',
+    members: '6465539d02739f6df0878cd3',
+    date: new Date('2023-02-12'),
+  },
+  {
+    _id: new mongoose.Types.ObjectId('6467f535fc13ae553d753af9'),
+    classId: '6466782ea1350cc48de5d438',
+    members: '6465539d02739f6df0878cd3',
+    date: new Date('2023-03-07'),
+  },
+  {
+    _id: new mongoose.Types.ObjectId('6467f535fc13ae553d753afa'),
+    classId: '6463a6281a8047b06aece8cf',
+    members: '64650c82fb9b0234915f4be7',
+    date: new Date('2023-02-25'),
+  },
+];
