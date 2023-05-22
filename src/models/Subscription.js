2e8aa8662e334659260795ad6127dd56bf7b549a
enriqueanastasini <enrianastasini@gmail.com>
2023-05-22 11:23:28 -0300

    MR-10-58: update of subscription schema and populate of member and class

--- src/models/Subscription.js
+++ src/models/Subscription.js
@@ -5,10 +5,12 @@ const { Schema } = mongoose;
 const subscriptionSchema = new Schema({
   classId: {
     type: Schema.Types.ObjectId,
+    ref: 'Class',
     required: true,
   },
   members: {
     type: Schema.Types.ObjectId,
+    ref: 'Member',
     required: true,
   },
   date: {
