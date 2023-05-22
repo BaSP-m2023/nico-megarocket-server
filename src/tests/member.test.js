1f63d77fc201630ad2792747b69231bb40a197d0
Daniel Ninfi <dbninfi@gmail.com>
2023-05-22 18:14:48 -0300

    MR-55: add more test for members test

--- src/tests/member.test.js
+++ src/tests/member.test.js
@@ -25,6 +25,48 @@ describe('POST /api/member', () => {
     expect(response.status).toBe(201);
     expect(response.error).toBeFalsy();
   });
+  test('returns an error if required fields are missing', async () => {
+    const data = {
+      firstName: '',
+      lastName: '',
+      dni: '',
+      birthday: '',
+      phone: '',
+      email: '',
+      city: '',
+      postalCode: '',
+      isActive: '',
+      membership: '',
+    };
+  
+    const response = await request(app).post('/api/member').send(data);
+  
+    expect(response.status).toBe(400);
+    expect(response.error).toBeTruthy();
+  });
+  test('returns an error if there is a server error', async () => {
+    jest.spyOn(Member, 'create').mockImplementation(() => {
+      throw new Error('Server Error');
+    });
+  
+    const data = {
+      firstName: 'sad222',
+      lastName: 'Fernandez',
+      dni: 37185888,
+      birthday: '2000-04-11T03:00:00.000Z',
+      phone: '5487965213',
+      email: 'nekro@yahoo.com',
+      city: 'Rosario',
+      postalCode: '2000',
+      isActive: true,
+      membership: 'Classic',
+    };
+  
+    const response = await request(app).post('/api/member').send(data);
+  
+    expect(response.status).toBe(500);
+    expect(response.error).toBeTruthy();
+  });
 });
 
 describe('GET /api/member', () => {
@@ -32,6 +74,19 @@ describe('GET /api/member', () => {
     const response = await request(app).get('/api/member').send();
     expect(response.status).toBe(200);
     expect(response.error).toBeFalsy();
+  });
+  test('returns status 500 when there is an internal server error', async () => {
+    jest.spyOn(Member, 'find').mockImplementation(() => {
+      throw new Error('Internal Server Error');
+    });
+    const response = await request(app).get('/api/member').send();
+    expect(response.status).toBe(500);
+  });
+  test('returns status 404 when no members are found', async () => {
+    jest.spyOn(Member, 'find').mockResolvedValue([]);
+    const response = await request(app).get('/api/members').send();
+    expect(response.status).toBe(404);
+    expect(response.error).toBeTruthy();
   });
 });
 
