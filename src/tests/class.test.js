7c9bc3dcc138409e222c60505df03198c36e6550
FlorFarruggia <[flor.farru@gmail.com]>
2023-05-23 12:35:40 -0300

    MR-37: Test for class get, get by id and post.

--- src/tests/class.test.js
+++ src/tests/class.test.js
@@ -1,5 +1,87 @@
-// Basic test to avoid conflicts
-// REMOVE THIS TEST AND MAKE YOURS!
-test('should pass a basic test', () => {
-  expect(1 + 1).toBe(2);
+import request from 'supertest';
+import app from '../app';
+import Class from '../models/Class';
+import classSeed from '../seeds/class';
+
+beforeAll(async () => {
+  await Class.collection.insertMany(classSeed);
+});
+
+describe('GET /api/class', () => {
+  test('This should return status 200', async () => {
+    const response = await request(app).get('/api/class').send();
+    expect(response.status).toBe(200);
+  });
+  test('Error should be false', async () => {
+    const response = await request(app).get('/api/class').send();
+    expect(response.error).toBeFalsy();
+  });
+});
+
+describe('GET BY ID /api/class/:id', () => {
+  test('This should return an Id', async () => {
+    const id = '64667748fc13ae7f027543d3';
+    const response = await request(app).get(`/api/class/${id}`).send();
+    expect(response.status).toBe(200);
+    expect(response.error).toBeFalsy();
+  });
+  test('This should return an Id invalid', async () => {
+    const id = '646596b54fcb63fdd73b28a5';
+    const response = await request(app).get(`/api/class/${id}`).send();
+    expect(response.status).toBe(404);
+    expect(response.error).toBeTruthy();
+  });
+  test('Check invalid Route', async () => {
+    const id = '646596b54fcb63fdd73b28a6';
+    const response = await request(app).get(`/api/classes/${id}`).send();
+    expect(response.status).toBe(404);
+    expect(response.error).toBeTruthy();
+  });
+});
+
+describe('POST /api/class', () => {
+  test('This should be error 500', async () => {
+    jest.spyOn(Class, 'create').mockImplementation(() => {
+      throw new Error('Error saving Class');
+    });
+    const mockClass = {
+      hour: '19:00',
+      day: 'Tuesday',
+      trainer: '646596b54fcb63fdd73b28a6',
+      activity: '64666b5d1fb62f8171bb6517',
+      slots: 9,
+    };
+    const response = await request(app).post('/api/class').send(mockClass);
+    expect(response.status).toBe(500);
+    expect(response.error).toBeTruthy();
+  });
+
+  test('This should create a class with status 201', async () => {
+    const mockClass = {
+      hour: '19:00',
+      day: 'Tuesday',
+      trainer: '646596b54fcb63fdd73b28a6',
+      activity: '64666b5d1fb62f8171bb6517',
+      slots: 9,
+    };
+    const response = await request(app).post('/api/class').send(mockClass);
+    expect(response.status).toBe(201);
+    expect(response.error).toBeFalsy();
+  });
+
+  test('This should return an error', async () => {
+    const mockClassTwo = {
+      hour: '05:45',
+      day: 'Fridays',
+      trainer: [
+        '646596b54fcb63fdd73b28a6',
+        '6465558202739f6df0878ceb',
+      ],
+      activity: '64662e65a1350cc48de5d410',
+      slots: 7,
+    };
+    const response = await request(app).post('/api/class').send(mockClassTwo);
+    expect(response.status).toBe(400);
+    expect(response.error).toBeTruthy();
+  });
 });
