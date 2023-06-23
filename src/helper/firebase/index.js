5dbb4c7c78a1fad69c6e11178083736531e30653
FrancoLelli <francogastonlelli@gmail.com>
2023-06-23 13:00:39 -0300

    MR-224: Update env names

--- src/helper/firebase/index.js
+++ src/helper/firebase/index.js
@@ -4,18 +4,18 @@ require('dotenv').config();
 
 const firebaseApp = admin.initializeApp({
   credential: admin.credential.cert({
-    type: process.env.FIREBASE_TYPE,
-    project_id: process.env.FIREBASE_PROJECT_ID,
-    private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID,
-    private_key: process.env.FIREBASE_PRIVATE_KEY,
-    client_email: process.env.FIREBASE_CLIENT_EMAIL,
-    client_id: process.env.FIREBASE_CLIENT_ID,
-    auth_uri: process.env.FIREBASE_AUTH_URI,
-    token_uri: process.env.FIREBASE_TOKEN_URI,
+    type: process.env.TYPE_FIREBASE,
+    project_id: process.env.PROJECT_ID_FIREBASE,
+    private_key_id: process.env.PRIVATE_KEY_ID_FIREBASE,
+    private_key: process.env.PRIVATE_KEY_FIREBASE,
+    client_email: process.env.CLIENT_EMAIL_FIREBASE,
+    client_id: process.env.CLIENT_ID_FIREBASE,
+    auth_uri: process.env.AUTH_URI_FIREBASE,
+    token_uri: process.env.TOKEN_URI_FIREBASE,
     auth_provider_x509_cert_url:
-      process.env.FIREBASE_AUTH_PROVIDER_X509_CERT_URL,
-    client_x509_cert_url: process.env.FIREBASE_CLIENT_X509_CERT_URL,
-    universe_domain: process.env.FIREBASE_UNIVERSE_DOMAIN,
+      process.env.AUTH_PROVIDER_X509_CERT_URL_FIREBASE,
+    client_x509_cert_url: process.env.CLIENT_X509_CERT_URL_FIREBASE,
+    universe_domain: process.env.UNIVERSE_DOMAIN_FIREBASE,
   }),
 });
 
