25d1cfdb75a1fb1a253bb3f182c49d50d07df530
Mateo Carciente <mateocarciente1@gmail.com>
2023-06-26 12:06:28 -0300

    MR-249: create super admin routes & imports

--- src/routes/super-admin.js
+++ src/routes/super-admin.js
@@ -1,16 +1,17 @@
 const express = require('express');
-const controllersSuperAdmin = require('../controllers/super-admin');
-const superAdminValidation = require('../validations/super-admin');
-
+const verifyToken = require('../middlewares/authMiddleware');
 const validationId = require('../validations/validateId');
+const adminValidate = require('../validations/admins');
 
 const router = express.Router();
 
+const controllersAdmin = require('../controllers/admins');
+
 router
-  .put('/:id', validationId.validateId, superAdminValidation.validateUpdate, controllersSuperAdmin.updateAdmin)
-  .get('/', controllersSuperAdmin.getAllSuperAdmin)
-  .get('/:id', validationId.validateId, controllersSuperAdmin.getSuperAdminById)
-  .post('/', superAdminValidation.createSuperAdminValidation, controllersSuperAdmin.createSuperAdmin)
-  .delete('/:id', validationId.validateId, controllersSuperAdmin.deleteSuperAdmin);
+  .put('/:id', verifyToken, validationId.validateId, adminValidate.validateUpdate, controllersAdmin.updateAdmin)
+  .get('/', verifyToken, controllersAdmin.getAdmins)
+  .get('/:id', verifyToken, validationId.validateId, controllersAdmin.getAdminsById)
+  .post('/', verifyToken, adminValidate.validateCreation, controllersAdmin.createAdmin)
+  .delete('/:id', verifyToken, validationId.validateId, controllersAdmin.deleteAdmin);
 
 module.exports = router;
